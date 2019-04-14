e591e65279d776c57dfaee3f892dfca3c7e3a48c
Manoj Tyagi <38884133+neo-0224@users.noreply.github.com>
2019-04-15 00:43:39 +0530

    Complete search functionality, pagination, results added

--- search.php
+++ search.php
@@ -1,6 +1,7 @@
 <?php
 
 include("config.php");
+include("classes/SiteResultsProvider.php");
 	
 
 	if (isset($_GET["term"])) {
@@ -12,6 +13,7 @@ include("config.php");
 
 
 	$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+	$page = isset($_GET["page"]) ? $_GET["page"] : 1;
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +65,72 @@ include("config.php");
 
 
 		<div class="mainResultsSection">
-			
+			<?php
+			$resultsProvider = new SiteResultsProvider($con);
+
+			$pageLimit = 20;
+
+			$numResults = $resultsProvider->getNumResults($term);
+
+			echo "<p class='resultsCount'>$numResults results found</p>";
+
+			echo $resultsProvider->getResultsHtml($page, $pageLimit, $term);
+			?>
+		</div>
+
+		<div class="paginationContainer">
+			<div class="pageButtons">
+				<div class="pageNumberContainer">
+					<img src="assets/img/pageStart.png" alt="PageStart">
+				</div>
+
+				<?php 
+
+				$pagesToShow = 10;
+				$numPages = ceil($numResults/$pageLimit);
+				$pagesLeft = min($pagesToShow, $numPages);
+
+				$currentPage = $page - floor($pagesToShow/2);
+
+				if ($currentPage < 1) {
+					$currentPage = 1;
+				}
+
+				if ($currentPage + $pagesLeft > $numPages +1) {
+					$currentPage = $numPages +1 - $pagesLeft;
+				}
+
+				while($pagesLeft != 0 && $currentPage <= $numPages) {
+					
+					if ($currentPage == $page) {
+						echo "<div class='pageNumberContainer'>
+							<img src='assets/img/pageSelected.png'>
+
+							<span class='pageNumber'><strong>$currentPage</strong></span>
+						</div>";
+					}
+
+					else {
+						echo "<div class='pageNumberContainer'>
+						<a href='search.php?term=$term&type=$type&page=$currentPage'>
+							<img src='assets/img/page.png'>
+
+							<span class='pageNumber'>$currentPage</span>
+							</a>
+						</div>";
+					}
+
+					$currentPage++;
+					$pagesLeft--;
+				} 
+
+				?>
+
+
+				<div class="pageNumberContainer">
+					<img src="assets/img/pageEnd.png" alt="PageEnd">
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
